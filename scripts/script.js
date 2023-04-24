56150e69ed84302d213cba901f2585677f317a47
chinchillajared <chinchillajared@gmail.com>
2023-04-24 11:27:07 -0600

    Javascript code added to display the year and the date

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById("current-year").textContent = `${year}| Jared Chinchilla Sanchez | Costa Rica`;
+document.getElementById("today").textContent = `Last updated: ${document.lastModified}`;
