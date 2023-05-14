96e927fa70bbc9027a8f4e8f0b5fcfbe54d50ddd
chinchillajared <chinchillajared@gmail.com>
2023-05-13 23:53:05 -0600

    Last changes to Chamber home page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,5 @@
+
+//Display the menu when the hamburger button is clicked
 function nav_menu(){
     document.getElementById('display-menu').classList.toggle('show');
 }
@@ -5,3 +7,20 @@ function nav_menu(){
 const hamburger_button = document.getElementById('hamburger');
 
 hamburger_button.onclick = nav_menu;
+
+
+//Show the current date
+const display_date = document.querySelector("time");
+
+const today = new Date();
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(today);
+
+display_date.textContent = fulldate;
+
+//Show the last update date
+const year = today.getFullYear();
+
+document.getElementById("current-year").textContent = `${year} Well Union Commerce Association`;
+
+document.getElementById("today").textContent = `Last updated: ${document.lastModified}`;
