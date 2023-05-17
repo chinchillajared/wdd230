9d2021632f5391902644b81f3dffc70a7004b659
chinchillajared <chinchillajared@gmail.com>
2023-05-16 22:48:14 -0600

    Banner added at the top of the page

--- lesson4/chamber/scripts/script.js
+++ lesson4/chamber/scripts/script.js
@@ -1,7 +1,7 @@
-
 //Display the menu when the hamburger button is clicked
 function nav_menu(){
     document.getElementById('display-menu').classList.toggle('show');
+    
 }
 
 const hamburger_button = document.getElementById('hamburger');
@@ -25,3 +25,23 @@ const year = today.getFullYear();
 document.getElementById("current-year").textContent = `${year} Well Union Commerce Association`;
 
 document.getElementById("today").textContent = `Last updated: ${document.lastModified}`;
+
+//Code tp shoe the banner at the top of the pages on Mondays or Tuesdays
+
+//Store the banner on a variable 
+const banner = document.getElementById('dynamic-banner')
+
+//Get the current day
+let day_of_week = today.getDay();
+
+//Conditional to show the banner depending of the day
+if (day_of_week == 1 || 2){
+    banner.classList.toggle('show');
+}
+
+//Remove the banner when the X button is pressed
+const remove_banner = document.getElementById('remove')
+
+remove_banner.addEventListener('click', function(){
+    banner.classList.toggle('hide');
+})
