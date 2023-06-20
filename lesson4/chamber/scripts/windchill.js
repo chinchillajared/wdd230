2b5708d342071c31980b973f6ce6ff27f83b4217
chinchillajared <chinchillajared@gmail.com>
2023-06-20 10:56:41 -0600

    Weather API data updated at the home page

--- lesson4/chamber/scripts/windchill.js
+++ lesson4/chamber/scripts/windchill.js
@@ -1,24 +1,58 @@
-//Get the Temperature from the weather section
-let temperature_value = document.getElementById('temperature').textContent;
-const f_temperature = parseInt(temperature_value) * 1.8 + 32;
+//Current weather (San Jose Costa Rica)
+//Set the latitude and longitud of the location from where you get the weather
+const lat = "9.93";
+const lon = "-84.10";
+const api_key = "be137bcff78136c8612eca7bc571f47f";
+const unit = "imperial";
 
-//Get the wind speed
-let wind_value = document.getElementById('wind-speed').textContent;
-const m_wind_speed = parseInt(wind_value) / 1.6093;
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + api_key + '&units=' + unit;
 
-//Specification limits  (<=50°F and >3.0mph)
-if (f_temperature >= 50 || m_wind_speed < 3){
-    document.getElementById('wind-child').innerText = 'N/A';
+//Get the weather data from the API
+async function getWeather() {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+
+    //Display the data
+    displayData(data);
 }
 
-else{
-    //Calculate the wind chill
-    const wind_chill =  13.12 + 0.6215 * f_temperature - 11.37 * m_wind_speed ** 0.16 + 0.3965 * f_temperature * m_wind_speed ** 0.16;
-    document.getElementById('wind-child').innerText = wind_chill.toFixed(1);
+getWeather();
+
+
+//Display the data
+function displayData(data) {
+
+    console.log(data);
+
+    //Distribute the data in variables
+    let temperature = data.main.temp;
+    let c_temperature = (temperature - 32) * 5 / 9;
+    document.getElementById('temperature').innerText = c_temperature.toFixed(1) + '°C';
+
+    let condition = data.weather[0].description;
+    document.querySelector(".weather-sec img").setAttribute("alt", condition);
+    document.querySelector(".condition").innerText = condition;
+
+    let icon = data.weather[0].icon;
+    document.querySelector(".weather-sec img").setAttribute("src", "https://openweathermap.org/img/w/" + icon + ".png");
+
+    let wind_speed = data.wind.speed;
+    let k_wind_speed = wind_speed * 1.60934;
+    document.getElementById('wind-speed').innerText = k_wind_speed.toFixed(1) + ' km/h';
+
+    //Specification limits  (<=50°F and >3.0mph)
+    if (temperature >= 50 || wind_speed < 3) {
+        document.getElementById('wind-child').innerText = 'N/A';
+    }
+
+    else {
+        //Calculate the wind chill
+        const wind_chill = 13.12 + 0.6215 * temperature - 11.37 * wind_speed ** 0.16 + 0.3965 * temperature * wind_speed ** 0.16;
+        document.getElementById('wind-child').innerText = wind_chill.toFixed(1);
+    }
+
 }
 
-//Current weather 
 
-//
 
 
